d228e788d3a051ecfc46f99f7f422f1d7c023299
Jamie <howemanning@gmail.com>
2023-05-24 04:25:05 +1200

    Add CSS reset and custom properties. Clean code.

--- script.js
+++ script.js
@@ -8,55 +8,46 @@ let computerScore = 0;
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 1) {
+    document.getElementById("score_card-2").className = "score_card-rock";
     return rock;
   } else if (randomNumber === 2) {
+    document.getElementById("score_card-2").className = "score_card-scissors";
     return scissors;
   } else {
+    document.getElementById("score_card-2").className = "score_card-paper";
     return paper;
   }
 }
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === rock && computerSelection === scissors) {
-    document.getElementById("score_card-1").className = "score_card-rock";
-    document.getElementById("score_card-2").className = "score_card-scissors";
     document.getElementById("score_user").innerHTML = ++playerScore;
     document.getElementById("summary").innerHTML =
-      "rock beats scissors, you win!";
+      "Rock beats scissors, you win!";
     return showWinner();
   } else if (playerSelection === rock && computerSelection === paper) {
-    document.getElementById("score_card-1").className = "score_card-rock";
-    document.getElementById("score_card-2").className = "score_card-paper";
     document.getElementById("score_computer").innerHTML = ++computerScore;
     document.getElementById("summary").innerHTML =
-      "paper beats rock, you lose!";
+      "Paper beats rock, you lose!";
     return showWinner();
   } else if (playerSelection === paper && computerSelection === scissors) {
-    document.getElementById("score_card-1").className = "score_card-paper";
-    document.getElementById("score_card-2").className = "score_card-scissors";
     document.getElementById("score_computer").innerHTML = ++computerScore;
     document.getElementById("summary").innerHTML =
-      "scissors beats paper, you lose!";
+      "Scissors beats paper, you lose!";
     return showWinner();
   } else if (playerSelection === paper && computerSelection === rock) {
-    document.getElementById("score_card-1").className = "score_card-paper";
-    document.getElementById("score_card-2").className = "score_card-rock";
     document.getElementById("score_user").innerHTML = ++playerScore;
-    document.getElementById("summary").innerHTML = "paper beats rock, you win!";
+    document.getElementById("summary").innerHTML = "Paper beats rock, you win!";
     return showWinner();
   } else if (playerSelection === scissors && computerSelection === rock) {
-    document.getElementById("score_card-1").className = "score_card-scissors";
-    document.getElementById("score_card-2").className = "score_card-rock";
     document.getElementById("score_computer").innerHTML = ++computerScore;
     document.getElementById("summary").innerHTML =
-      "rock beats scissors, you lose!";
+      "Rock beats scissors, you lose!";
     return showWinner();
   } else if (playerSelection === scissors && computerSelection === paper) {
-    document.getElementById("score_card-1").className = "score_card-scissors";
-    document.getElementById("score_card-2").className = "score_card-paper";
     document.getElementById("score_user").innerHTML = ++playerScore;
     document.getElementById("summary").innerHTML =
-      "scissors beats paper, you win!";
+      "Scissors beats paper, you win!";
     return showWinner();
   } else {
     document.getElementById("summary").innerHTML = "Draw!";
@@ -65,6 +56,7 @@ function playRound(playerSelection, computerSelection) {
 
 const getRockBtn = document.querySelector("#rock_btn");
 getRockBtn.addEventListener("click", () => {
+  document.getElementById("score_card-1").className = "score_card-rock";
   computerSelection = getComputerChoice();
   playerSelection = rock;
   return playRound(playerSelection, computerSelection);
@@ -72,6 +64,7 @@ getRockBtn.addEventListener("click", () => {
 
 const getScissorsBtn = document.querySelector("#scissors_btn");
 getScissorsBtn.addEventListener("click", () => {
+  document.getElementById("score_card-1").className = "score_card-scissors";
   computerSelection = getComputerChoice();
   playerSelection = scissors;
   return playRound(playerSelection, computerSelection);
@@ -79,6 +72,7 @@ getScissorsBtn.addEventListener("click", () => {
 
 const getPaperBtn = document.querySelector("#paper_btn");
 getPaperBtn.addEventListener("click", () => {
+  document.getElementById("score_card-1").className = "score_card-paper";
   computerSelection = getComputerChoice();
   playerSelection = paper;
   return playRound(playerSelection, computerSelection);
@@ -87,18 +81,18 @@ getPaperBtn.addEventListener("click", () => {
 document.getElementById("score_user").innerHTML = playerScore;
 document.getElementById("score_computer").innerHTML = computerScore;
 
-function showWinner() {
-  if (playerScore === 5) {
-    document.getElementById("winner").innerHTML = "You win!";
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById("score_user").innerHTML = playerScore;
-    document.getElementById("score_computer").innerHTML = computerScore;
-  } else if (computerScore === 5) {
-    document.getElementById("winner").innerHTML = "You lose!";
-    playerScore = 0;
-    computerScore = 0;
-    document.getElementById("score_user").innerHTML = playerScore;
-    document.getElementById("score_computer").innerHTML = computerScore;
-  }
-}
+// function showWinner() {
+//   if (playerScore === 5) {
+//     document.getElementById("winner").innerHTML = "You win!";
+//     playerScore = 0;
+//     computerScore = 0;
+//     document.getElementById("score_user").innerHTML = playerScore;
+//     document.getElementById("score_computer").innerHTML = computerScore;
+//   } else if (computerScore === 5) {
+//     document.getElementById("winner").innerHTML = "You lose!";
+//     playerScore = 0;
+//     computerScore = 0;
+//     document.getElementById("score_user").innerHTML = playerScore;
+//     document.getElementById("score_computer").innerHTML = computerScore;
+//   }
+// }
