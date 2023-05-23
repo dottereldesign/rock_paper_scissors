7ec92f49b24e2c2d37e44552ed0ed888b1eed5ba
Jamie <howemanning@gmail.com>
2023-05-24 02:57:06 +1200

    Append current hand img to score.

--- script.js
+++ script.js
@@ -16,32 +16,57 @@ function getComputerChoice() {
   }
 }
 
+// function currentHand() {
+//   let currentRock = document.getElementbyID;
+//   if (playerSelection === rock) {
+//     document.getElementById("score_card-1").classList.add("score_card-rock");
+//     document
+//       .getElementById("score_card-2")
+//       .classList.add("score_card-scissors");
+//     var el = document.getElementById("divID");
+//     // Removing class
+//     el.classList.remove("my-class");
+//   }
+// }
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === rock && computerSelection === scissors) {
+    document.getElementById("score_card-1").className = "score_card-rock";
+    document.getElementById("score_card-2").className = "score_card-scissors";
     document.getElementById("score_user").innerHTML = ++playerScore;
     document.getElementById("summary").innerHTML =
       "rock beats scissors, you win!";
     return showWinner();
   } else if (playerSelection === rock && computerSelection === paper) {
+    document.getElementById("score_card-1").className = "score_card-rock";
+    document.getElementById("score_card-2").className = "score_card-paper";
     document.getElementById("score_computer").innerHTML = ++computerScore;
     document.getElementById("summary").innerHTML =
       "paper beats rock, you lose!";
     return showWinner();
   } else if (playerSelection === paper && computerSelection === scissors) {
+    document.getElementById("score_card-1").className = "score_card-paper";
+    document.getElementById("score_card-2").className = "score_card-scissors";
     document.getElementById("score_computer").innerHTML = ++computerScore;
     document.getElementById("summary").innerHTML =
       "scissors beats paper, you lose!";
     return showWinner();
   } else if (playerSelection === paper && computerSelection === rock) {
+    document.getElementById("score_card-1").className = "score_card-paper";
+    document.getElementById("score_card-2").className = "score_card-rock";
     document.getElementById("score_user").innerHTML = ++playerScore;
     document.getElementById("summary").innerHTML = "paper beats rock, you win!";
     return showWinner();
   } else if (playerSelection === scissors && computerSelection === rock) {
+    document.getElementById("score_card-1").className = "score_card-scissors";
+    document.getElementById("score_card-2").className = "score_card-rock";
     document.getElementById("score_computer").innerHTML = ++computerScore;
     document.getElementById("summary").innerHTML =
       "rock beats scissors, you lose!";
     return showWinner();
   } else if (playerSelection === scissors && computerSelection === paper) {
+    document.getElementById("score_card-1").className = "score_card-scissors";
+    document.getElementById("score_card-2").className = "score_card-paper";
     document.getElementById("score_user").innerHTML = ++playerScore;
     document.getElementById("summary").innerHTML =
       "scissors beats paper, you win!";
